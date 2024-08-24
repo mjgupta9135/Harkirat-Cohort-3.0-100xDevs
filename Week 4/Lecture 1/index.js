4af039d10ab779e90472e5246e1455d72d44ba98
Mrityunjay <mjguptacse@gmail.com>
2024-08-24 21:02:27 +0530

    Node JS Started

---
+++ Week 4/Lecture 1/index.js
@@ -0,0 +1,7 @@
+/**
+ * ! What is NPM?
+ * * NPM is a package manager for javascript primarly used for managing external dependencies,libraries.
+ */
+
+import chalk from "chalk";
+console.log(chalk);
