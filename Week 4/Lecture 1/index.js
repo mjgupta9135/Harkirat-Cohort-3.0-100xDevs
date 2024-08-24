d0e3a96a0547c5ee644a4b931b457ae3dd884fd8
Mrityunjay <mjguptacse@gmail.com>
2024-08-24 22:15:19 +0530

    created CLI using commander package

--- Week 4/Lecture 1/index.js
+++ Week 4/Lecture 1/index.js
@@ -1,7 +1,30 @@
-/**
- * ! What is NPM?
- * * NPM is a package manager for javascript primarly used for managing external dependencies,libraries.
- */
+const fs = require("fs");
+const { Command } = require("commander");
+const program = new Command();
 
-import chalk from "chalk";
-console.log(chalk);
+program
+  .name("counter")
+  .description("CLI to do file based tasks")
+  .version("0.8.0");
+
+program
+  .command("count")
+  .description("Count the number of words in a file")
+  .argument("<file>", "file to count")
+  .action((file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let words = 0;
+        for (let i = 0; i < data.length; i++) {
+          if (data[i] === " ") {
+            words++;
+          }
+        }
+
+        console.log(`There are ${words + 1} lines in ${file}`);
+      }
+    });
+  });
+program.parse(process.argv);
--- Week 4/Lecture 1/index.mjs
+++
@@ -1,4 +0,0 @@
-import chalk from "chalk";
-console.log(chalk.blue("Hello Guys"));
-console.log(chalk.red.italic("This is a error message"));
-console.log(chalk.green.bold("This is a success message"));
