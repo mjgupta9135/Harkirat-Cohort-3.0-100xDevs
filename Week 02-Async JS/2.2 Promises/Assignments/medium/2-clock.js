17c2dd15017dc63d3c8d6a00da7a9f2c3fb81f49
Mrityunjay <mjguptacse@gmail.com>
2024-08-25 15:02:23 +0530

    Promises Medium Assignment Completed

--- Week 02-Async JS/2.2 Promises/Assignments/medium/2-clock.js
+++ Week 02-Async JS/2.2 Promises/Assignments/medium/2-clock.js
@@ -19,11 +19,23 @@ function timeDisplay() {
     if (currentTime.hours > 23) {
       currentTime.hours = 0;
     }
+    let str = "";
+    if (currentTime.hours < 12) {
+      str = "AM";
+    } else {
+      str = "PM";
+    }
     let formattedHours = String(currentTime.hours).padStart(2, "0");
     let formattedMinutes = String(currentTime.minute).padStart(2, "0");
     let formattedSeconds = String(currentTime.second).padStart(2, "0");
     console.log(
-      formattedHours + ":" + formattedMinutes + ":" + formattedSeconds
+      formattedHours +
+        ":" +
+        formattedMinutes +
+        ":" +
+        formattedSeconds +
+        " " +
+        str
     );
   }, 1000);
 }
