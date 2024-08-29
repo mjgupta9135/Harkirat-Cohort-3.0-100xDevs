9250fa686d88865f29387cb1215178737c2a2f89
Mrityunjay <mjguptacse@gmail.com>
2024-08-30 01:05:01 +0530

    State and Component half completed

--- Week-03-DOM/3.1-DOM-(Simple)/index.js
+++ Week-03-DOM/3.1-DOM-(Simple)/index.js
@@ -4,3 +4,20 @@
  * DOM abstracts the structure of the document into a tree of objects,allowing scripts to manipulate the content and structure dynamically.
  * This abstraction enables more complex interactions and functionalities beyond just static HTML.
  */
+let count = 0;
+function addTodo() {
+  const parent = document.querySelector("#content");
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("todo-style", "todo-" + count);
+  newDiv.innerHTML =
+    '<div class="todo-head"> <input type="checkbox" class="check"><div class="extra"><h1 class="task' +
+    count +
+    '"></h1></div></div><div class="todo-btn><button><i class"fa-solid fa-trash-can icon"/></button></div>';
+  parent.appendChild(newDiv);
+  let inputVal = document.querySelector(".input-box").value;
+  const task = document.querySelector(".task" + count);
+  task.innerHTML = inputVal;
+  console.log(newDiv);
+  inputVal = "";
+  count++;
+}
