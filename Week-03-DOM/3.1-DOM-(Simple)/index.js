74bb01286ee15d983aa2390a735fb8369ca0c63f
Mrityunjay <mjguptacse@gmail.com>
2024-08-29 12:51:19 +0530

    Todo Project is intialized delete button is working

---
+++ Week-03-DOM/3.1-DOM-(Simple)/index.js
@@ -0,0 +1,6 @@
+/*
+ * DOM:- Document Object Model is a programming interface for web documents.
+ * It represents the structure of a web page as a tree of objects.
+ * DOM abstracts the structure of the document into a tree of objects,allowing scripts to manipulate the content and structure dynamically.
+ * This abstraction enables more complex interactions and functionalities beyond just static HTML.
+ */
