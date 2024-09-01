c8d5a65a333c8511fa0e8dabc5c14ad500c3e2cf
Mrityunjay <mjguptacse@gmail.com>
2024-09-01 20:50:40 +0530

    Created middleware

---
+++ Week-05-HTTP-Headers-Middlewares/5.2-Middleware/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+let requestCount = 0;
+function print(req, res, next) {
+  console.log({
+    url: req.hostname,
+    method: req.method,
+    currentTime: new Date(),
+  });
+  next();
+}
+function requestIncrease(res, req, next) {
+  requestCount++;
+  console.log(req);
+  next();
+}
+
+app.get("/sum/:a/:b", requestIncrease, function (req, res) {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+
+  res.json({
+    ans: a + b,
+  });
+});
+app.get("/multiply/:a/:b", requestIncrease, function (req, res) {
+  const a = parseInt(req.params.a);
+  const b = parseInt(req.params.b);
+
+  res.json({
+    ans: a * b,
+  });
+});
+
+app.get("/admin", print, function (req, res) {
+  res.json({
+    message: "Total no of requests are:- " + requestCount,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on port no 3000");
+});
